005aa832fce0b3a9635529fe68ffc7608c1a249f
chacha <89715044+chachapp@users.noreply.github.com>
2023-08-18 14:39:16 +0900

    fix: JPA Config 주석, secret-key, datasource 환경변수 설정

--- src/main/java/com/github/backend1st/config/JwtTokenProvider.java
+++ src/main/java/com/github/backend1st/config/JwtTokenProvider.java
@@ -6,12 +6,14 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Base64;
 import java.util.Date;
@@ -19,7 +21,17 @@ import java.util.Date;
 @Component
 @RequiredArgsConstructor
 public class JwtTokenProvider {
-    private String secretKey = Base64.getEncoder().encodeToString("be_commerce_1".getBytes());
+
+    @Value("${jwt.secret-key-source}")
+    private String secretKeySource;
+
+    private String secretKey;
+
+    @PostConstruct
+    public void setUp() {
+        secretKey = Base64.getEncoder().encodeToString(secretKeySource.getBytes());
+    }
+
     private long tokenValidMillisecond = 1000L * 60 * 60; // 1시간
 
     private final CustomUserDetailsService customUserDetailsService;
