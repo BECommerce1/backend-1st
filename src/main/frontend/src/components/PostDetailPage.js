d96a73014dc859933fe31f4e8d9695626af2d185
chacha <89715044+chachapp@users.noreply.github.com>
2023-08-18 16:45:44 +0900

    fix: 좋아요 프론트 수정

--- src/main/frontend/src/components/PostDetailPage.js
+++ src/main/frontend/src/components/PostDetailPage.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {Card, CardContent, TextField, Typography} from '@mui/material';
 import {red} from '@mui/material/colors';
 import {blue, CustomButton} from './CustomButton';
@@ -6,28 +6,16 @@ import {StyledTextarea} from './StyledTextArea';
 import {useNavigate} from 'react-router-dom';
 
 const PostDetailPage = () => {
+  const [likeText, setLikeText] = useState('좋아요');
   const navigate = useNavigate();
-  const [post, setPost] = useState({});
+  const [post, setPost] = useState({
+
+
+  });
   const [comments, setComments] = useState([{
-      id: 1,
-      content: 1,
-      author: '작성자1',
-      post_id: 3,
-      created_at: '작성일시'
-    },
-    {
-      id: 2,
-      content: 1,
-      author: '작성자2',
-      post_id: 1,
-      created_at: '작성일시'
-    },
-    {
-      id: 3,
-      content: 1,
-      author: '작성자3',
-      post_id: 2,
-      created_at: '작성일시'
+
+
+
     }]);
   const [like, setLike] = useState(false)
 
@@ -39,21 +27,13 @@ const PostDetailPage = () => {
   })
 
   async function fetchData() {
-    await fetch('http://localhost:8080/api/comments')
-    .then(res => res.json()).then(res => {
-      if(!res) return;
-      setComments([...res.comments.filter(c => c?.post_id === post.id)])
-    })
-    .catch((err) => console.error(err));
+    // await fetch('http://localhost:8080/api/comments')
+    // .then(res => res.json()).then(res => {
+    //   if(!res) return;
+    //   setComments([...res.comments.filter(c => c?.post_id === post.id)])
+    // })
+    // .catch((err) => console.error(err));
   }
-
-
-  const fetchComment = async (id,post_id) => {
-    const response = await fetch(`/v1/api/check_like?memberId=${id}&replyId=${post_id}`);
-    const data = await response.json();
-    return data;
-  };
-
 
   useEffect(() => {
     const postData = JSON.parse(localStorage.getItem('post'));
@@ -86,43 +66,83 @@ const PostDetailPage = () => {
   };
 
 
-  const handleCommentInsert = async (id, post_id) => {
-    const comment = await fetchComment(id,post_id);
+  const checklikesHandler = async (id,post_id) => {
+    post_id = 1;
+
+    console.log(id , post_id)
+
+    console.log(localStorage.getItem('X-AUTH-TOKEN'))
+
+    const response=
+        await fetch(`http://localhost:8080/v1/api/check_like?replyId=${post_id}`, {
+          method: 'GET',
+          headers : {
+            'X-AUTH-TOKEN' : localStorage.getItem('X-AUTH-TOKEN')
+          }
+    });
+
+    console.log('checklikesHandler : ' + response.status)
+
+    const data = await response.json();
+    return data;
+  };
+
+
+  const handleLikeInsert = async (id, post_id) => {
+    post_id = 1;
+    // 댓글에 이미 좋아요가 있는지 없는지..
+    const comment = await checklikesHandler(id,post_id);
+
+    console.log('handleLikeInsert : ' + comment)
+
     if (comment == false) {
-      // 댓글에 no가 없을 경우
-      const response = await fetch(`/v1/api/insert_like`, {
+      // 댓글에 좋아요가 없을 경우.
+      const response = await fetch(`http://localhost:8080/v1/api/insert_like/${id}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Referrer-Policy': 'no-referrer'
+          'Referrer-Policy': 'no-referrer',
+          'X-AUTH-TOKEN' : localStorage.getItem('X-AUTH-TOKEN')
         },
         body: JSON.stringify({
-          memberId: id,
-          replyId: post_id
+         // replyId: id
         })
-      });
+      }).catch((error) => alert(error));
+
+      console.log('handleLikeInsert_response : ' + response.status)
+
       if (response.status === 200) {
+        console.log(response.status)
         // 좋아요가 성공적으로 추가된 경우
-        document.getElementById(post_id).innerHTML = '취소';
-        document.getElementById(post_id).style.backgroundColor = 'red';
+        setLikeText('취소');
+
+        // likeRef.current. = '취소';
+        //document.getElementById().innerHTML = '취소';
+        //document.getElementById(post_id).style.backgroundColor = 'red';
       }
     } else {
-      // 댓글에 no가 있을 경우
-      const response = await fetch(`/v1/api/delete_like/${id}/${post_id}`, {
+      // 댓글에 좋아요가 있을 경우
+      const response = await fetch(`http://localhost:8080/v1/api/delete_like/${id}`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
-          'Referrer-Policy': 'no-referrer'
+          'Referrer-Policy': 'no-referrer',
+          'X-AUTH-TOKEN' : localStorage.getItem('X-AUTH-TOKEN')
         },
         body: JSON.stringify({
-          memberId: id,
-          replyId: post_id
+          // replyId: post_id
         })
       });
       if (response.status === 200) {
+        console.log('delete : ' + response.status)
+
         // 좋아요가 성공적으로 삭제된 경우
-        document.getElementById(post_id).innerHTML = '좋아요';
-        document.getElementById(post_id).style.backgroundColor = 'rgb(0, 114, 229)';
+        setLikeText('좋아요');
+        // document.getElementById(post_id).innerHTML = '좋아요';
+        // document.getElementById(post_id).style.backgroundColor = 'rgb(0, 114, 229)';
+      }
+      else {
+        console.log('handleLikeInsert : ' + response.status)
       }
     }
   };
@@ -143,15 +163,19 @@ const PostDetailPage = () => {
   const submitComment = async () => {
     await fetch(`http://localhost:8080/api/comments`, {
       method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
       body: JSON.stringify({
         author: newComment.author,
         content: newComment.content,
-        post_id: post.id
+        post_id: post.id // postId
       })
     }).catch((err) => console.error(err));
   }
   const likes = comments.map(c => c.id);
 
+  // TODO :
   const isLiked = async(id, post_id) => {
     const response = await fetch(`/v1/api/check_like?memberId=${id}&replyId=${post_id}`, {
       method: 'GET',
@@ -229,8 +253,7 @@ const PostDetailPage = () => {
                 </Typography>
                 <CustomButton style={{ backgroundColor: blue[500] }} onClick={() => handleCommentChange(c.id, c.content)}>수정</CustomButton>
                 {isLiked(sessionId, c.post_id) ? "" : ""}
-                <CustomButton id={c.id} class="like-button" style={{ marginTop: 10 }} onClick={() => handleCommentInsert(sessionId, c.id)}>좋아요</CustomButton>
-
+                <CustomButton id={c.id} class="like-button" style={{ marginTop: 10 }} onClick={() => handleLikeInsert(sessionId, c.id)}> {likeText} </CustomButton>
               </CardContent>
             </Card>
           )))
